a195ee2be09b1176585f2b286eac9bc6b0e25e7a
Espen Meidell <espen.meidell@gmail.com>
2018-02-20 10:51:05 +0100

    Can evolve

--- Genetics.go
+++ Genetics.go
@@ -3,30 +3,58 @@ package main
 import (
 	"math/rand"
 	"sort"
-	"fmt"
 )
 
 // Mutate a solution
 func (solution *Solution) mutate() {
 	i, j := rand.Intn(len(solution.cities)), rand.Intn(len(solution.cities))
 	switch rand.Intn(2) {
-	case 0:		// Swap mutation
+	case 0: // Swap mutation
 		solution.cities[i], solution.cities[j] = solution.cities[j], solution.cities[i]
-	case 1:		// Reverse mutation
+	case 1: // Reverse mutation
 		indices := []int{i, j}
 		sort.Ints(indices)
-		for i := indices[1]/2-1; i >= indices[0]; i-- {
-			opp := indices[1]-1-i
+		for i := indices[1]/2 - 1; i >= indices[0]; i-- {
+			opp := indices[1] - 1 - i
 			solution.cities[i], solution.cities[opp] = solution.cities[opp], solution.cities[i]
 		}
 	}
 	solution.Distance = calculateDistance(solution.cities)
 }
 
-func crossover(mama, papa *Solution)  {
+func crossover(mama, papa *Solution) (Solution, Solution) {
 	cities1 := make([]City, len(mama.cities))
 	cities2 := make([]City, len(papa.cities))
 	copy(cities1, mama.cities)
 	copy(cities2, papa.cities)
+	child1 := Solution{cities1, calculateDistance(cities1)}
+	child2 := Solution{cities2, calculateDistance(cities2)}
+	child1.mutate()
+	child2.mutate()
+	return child1, child1
+}
 
+func getParents(population []Solution) (Solution, Solution) {
+	tournament := []Solution{
+		population[rand.Intn(len(population))],
+		population[rand.Intn(len(population))],
+		population[rand.Intn(len(population))],
+		population[rand.Intn(len(population))]}
+	sort.Sort(ByDistance(tournament))
+	return tournament[0], tournament[1]
+}
+
+func evolve(population []Solution) []Solution {
+	newPopulation := make([]Solution, 0)
+	for len(newPopulation) < len(population) {
+		mama, papa := getParents(population)
+		child1, child2 := crossover(&mama, &papa)
+		child1.mutate()
+		child2.mutate()
+		newPopulation = append(newPopulation, child1)
+		newPopulation = append(newPopulation, child2)
+	}
+	newPopulation = append(newPopulation, population...)
+	sort.Sort(ByDistance(newPopulation))
+	return newPopulation[:len(population)]
 }
--- Main.go
+++ Main.go
@@ -9,25 +9,17 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	cities := readProblemFile("data/6.txt")
-	population := make([]Solution, 5)
+	cities := readProblemFile("data/1.txt")
+	population := make([]Solution, 10)
 	for i := 0; i < len(population); i++ {
 		population[i] = createRandomSolution(cities)
 	}
 
 
-	//for i := range population {
-	//	population[i].mutate()
-	//}
-	//sort.Sort(ByDistance(population))
-
-	s1 := createRandomSolution(cities)
-	s2 := createRandomSolution(cities)
-
-	fmt.Println("S1", s1)
-	fmt.Println("S2", s2)
-
-	crossover(&s1, &s2)
+	for i := 0; i < 100000; i++ {
+		population = evolve(population)
+		fmt.Println(population[0].Distance)
+	}
 
 
 }
